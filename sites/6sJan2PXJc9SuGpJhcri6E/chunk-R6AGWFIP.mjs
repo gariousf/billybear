7d7cfa1ef9298d7abd595c74831fec91be308c19
Greg Fitzgerald <greg@solana.com>
2025-03-10 05:06:02 +0200

    setup

---
+++ sites/6sJan2PXJc9SuGpJhcri6E/chunk-R6AGWFIP.mjs
@@ -0,0 +1,4 @@
+function t(a,e){return{customHTMLBodyStart:`<script>
+  document.body.style.overscrollBehavior = "none";
+<\/script>`,favicon:"https://framerusercontent.com/assets/ejRwUECc7dfQHSikH1qRpp5eE.png",robots:"max-image-preview:large",title:"$GRUMS"}}export{t as a};
+//# sourceMappingURL=chunk-R6AGWFIP.mjs.map
